bcecdb7ab88609bf04528a0f274c821d3a5d1452
robbin3105 <200546@student.glu.nl>
2021-02-03 12:49:21 +0100

    Topdown 1

---
+++ Assets/Scripts/TopdownScripts/HumanBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanBehaviour : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private GameObject plr;
+
+    [SerializeField] private float run_distance;
+
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, plr.transform.position);
+
+        Vector3 dir_to_plr = transform.position - plr.transform.position;
+
+        if (distance < run_distance)
+        {
+            transform.Translate(dir_to_plr * speed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TopdownScripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    private Vector2 input;
+
+    [SerializeField] private float speed = 5;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = (input * speed);
+    }
+}
